d5e9c3d6a94b9839fdd2071b8d768a0c2fce1588
madhav <eragon.cube@outlook.com>
2023-11-02 23:44:59 +0530

    insert and delete in admin table :)

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('railwayproject', 'root', 'Shubhangi@2004', {
+const db = new Sequelize('railwayproject', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 
